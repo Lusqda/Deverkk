d722f4d1cde3298908688e124284e8fe8931c92a
Lusqda <luka244ifrj@gmail.com>
2025-05-09 14:35:20 -0300

    soma dois numeros e metros milimetros

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -1,6 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Aula01._1bim;
 using System.Runtime.InteropServices;
 
+bool continuar = true;
+while (continuar) {
+    Console.WriteLine("//MENU//\n");
+    Console.WriteLine("\nESCOLHA UMA OPÇÃO:\n");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("1 - Somar dois números");
+    Console.WriteLine("2 - Converter metros(m) para milímetros(mm)");
+    Console.WriteLine("3 - Calcular aumento de salário");
+    int opcao = int.Parse(Console.ReadLine());
+    switch (opcao)
+    {
+        case 0:
+            Console.WriteLine("Saindo...");
+            continuar = false;
+            break;
+        case 1:
+            Console.WriteLine("Escreva dois números: ");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            SomaDoisNumeros somador = new SomaDoisNumeros();
+            int resultado = somador.Somar(num1, num2);
+            Console.WriteLine($"A soma dos números {num1} e {num2} é {resultado}");
+            break;
+        case 2:
+            Console.WriteLine("Escreva o comprimento(m): ");
+            int metros = int.Parse(Console.ReadLine());
+            MetrosMilimetros converta = new MetrosMilimetros();
+            int milimetros = converta.Converter(metros);
+            Console.WriteLine($"{metros}m é igual a {milimetros}mm");
+            break;
+    }
+}
+
+/*
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Informe seu nome: ");
 string nome = Console.ReadLine();
@@ -46,4 +81,4 @@ catch(Exception e)
 {
     Console.WriteLine("Ocorreu um erro desconhecido, contate o desenvolvedor.");
 }
-
+*/
